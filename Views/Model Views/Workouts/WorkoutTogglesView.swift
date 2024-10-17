7c776c8d2bbc31c0c5cf2a8a7d7749d4cd6d3ec7
zkralec <zkralec@icloud.com>
2024-10-17 10:13:54 -0400

    Misc: Major reogranization of files

---
+++ Views/Model Views/Workouts/WorkoutTogglesView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutTogglesView.swift
+//  Track Tracker
+//
+//  Created by Zachary Kralec on 10/17/24.
+//
+
+import SwiftUI
+
+struct WorkoutTogglesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutTogglesView()
+}
