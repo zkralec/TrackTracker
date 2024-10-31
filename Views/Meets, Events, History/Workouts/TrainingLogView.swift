913be6c797f34c5bd93fb76756f0ed54db7141e3
zkralec <zkralec@icloud.com>
2024-10-31 12:01:54 -0400

    Update: Removing TrainingLogView's Extras button if there are none

--- Views/Meets, Events, History/Workouts/TrainingLogView.swift
+++ Views/Meets, Events, History/Workouts/TrainingLogView.swift
@@ -94,87 +94,89 @@ struct TrainingLogView: View {
                                                     .padding(.top, 3)
                                             }
                                         }
-                                        // Extras section
-                                        DisclosureGroup("Extra Info.") {
-                                            // Surfaces
-                                            ForEach([
-                                                (key: "Track", value: workout.track),
-                                                (key: "Indoor Track", value: workout.indoorTrack),
-                                                (key: "Turf", value: workout.turf),
-                                                (key: "Dirt", value: workout.dirt),
-                                                (key: "Grass/Hills", value: workout.grasshills),
-                                                (key: "Asphalt", value: workout.asphalt)
-                                            ], id: \.key) { surface in
-                                                if surface.value {
-                                                    Text("Surface: \(surface.key)")
+                                        if workout.hasExtras {
+                                            // Extras section
+                                            DisclosureGroup("Extra Info.") {
+                                                // Surfaces
+                                                ForEach([
+                                                    (key: "Track", value: workout.track),
+                                                    (key: "Indoor Track", value: workout.indoorTrack),
+                                                    (key: "Turf", value: workout.turf),
+                                                    (key: "Dirt", value: workout.dirt),
+                                                    (key: "Grass/Hills", value: workout.grasshills),
+                                                    (key: "Asphalt", value: workout.asphalt)
+                                                ], id: \.key) { surface in
+                                                    if surface.value {
+                                                        Text("Surface: \(surface.key)")
+                                                    }
                                                 }
-                                            }
-                                            // Weather
-                                            let weatherOptions = [
-                                                (key: "Rain", value: workout.rain),
-                                                (key: "Snow", value: workout.snow),
-                                                (key: "Windy", value: workout.windy),
-                                                (key: "Normal", value: workout.normal),
-                                                (key: "Hot", value: workout.hot),
-                                                (key: "Cold", value: workout.cold)
-                                            ]
-                                            let selectedWeather = weatherOptions.filter { $0.value }.map { $0.key }.joined(separator: ", ")
-                                            if selectedWeather != "" {
-                                                Text("Weather: \(selectedWeather)")
-                                            }
-                                            // Equipment
-                                            let equipmentOptions = [
-                                                (key: "Blocks", value: workout.blocks),
-                                                (key: "Resistance Band", value: workout.resistanceBand),
-                                                (key: "Weights", value: workout.weights),
-                                                (key: "Sled", value: workout.sled),
-                                                (key: "Hurdles", value: workout.hurdles),
-                                                (key: "Weighted Vest", value: workout.weightedVest),
-                                                (key: "Plyo Box", value: workout.plyoBox),
-                                                (key: "Medicine Ball", value: workout.medicineBall),
-                                                (key: "Stationary Bike", value: workout.stationaryBike),
-                                                (key: "Treadmill", value: workout.treadmill)
-                                            ]
-                                            let selectedEquipment = equipmentOptions.filter { $0.value }.map { $0.key }.joined(separator: ", ")
-                                            if selectedEquipment != "" {
-                                                Text("Equipment: \(selectedEquipment)")
-                                            }
-                                            // Condition
-                                            ForEach([
-                                                (key: "Injury", value: workout.injury),
-                                                (key: "Soreness", value: workout.soreness),
-                                                (key: "Fatigued", value: workout.fatigued),
-                                                (key: "Peak Form", value: workout.peakForm)
-                                            ], id: \.key) { condition in
-                                                if condition.value {
-                                                    Text("Condition: \(condition.key)")
+                                                // Weather
+                                                let weatherOptions = [
+                                                    (key: "Rain", value: workout.rain),
+                                                    (key: "Snow", value: workout.snow),
+                                                    (key: "Windy", value: workout.windy),
+                                                    (key: "Normal", value: workout.normal),
+                                                    (key: "Hot", value: workout.hot),
+                                                    (key: "Cold", value: workout.cold)
+                                                ]
+                                                let selectedWeather = weatherOptions.filter { $0.value }.map { $0.key }.joined(separator: ", ")
+                                                if selectedWeather != "" {
+                                                    Text("Weather: \(selectedWeather)")
                                                 }
-                                            }
-                                            // Intensity
-                                            ForEach([
-                                                (key: "Low", value: workout.low),
-                                                (key: "Moderate", value: workout.moderate),
-                                                (key: "High", value: workout.high),
-                                                (key: "Maximum", value: workout.maximum)
-                                            ], id: \.key) { intensity in
-                                                if intensity.value {
-                                                    Text("Intensity: \(intensity.key)")
+                                                // Equipment
+                                                let equipmentOptions = [
+                                                    (key: "Blocks", value: workout.blocks),
+                                                    (key: "Resistance Band", value: workout.resistanceBand),
+                                                    (key: "Weights", value: workout.weights),
+                                                    (key: "Sled", value: workout.sled),
+                                                    (key: "Hurdles", value: workout.hurdles),
+                                                    (key: "Weighted Vest", value: workout.weightedVest),
+                                                    (key: "Plyo Box", value: workout.plyoBox),
+                                                    (key: "Medicine Ball", value: workout.medicineBall),
+                                                    (key: "Stationary Bike", value: workout.stationaryBike),
+                                                    (key: "Treadmill", value: workout.treadmill)
+                                                ]
+                                                let selectedEquipment = equipmentOptions.filter { $0.value }.map { $0.key }.joined(separator: ", ")
+                                                if selectedEquipment != "" {
+                                                    Text("Equipment: \(selectedEquipment)")
                                                 }
-                                            }
-                                            // Field events
-                                            let eventOptions = [
-                                                (key: "High Jump", value: workout.highJump),
-                                                (key: "Pole Vault", value: workout.poleVault),
-                                                (key: "Hammer Throw", value: workout.hammerThrow),
-                                                (key: "Discus", value: workout.discus),
-                                                (key: "Shot Put", value: workout.shotPut),
-                                                (key: "Javelin", value: workout.javelin),
-                                                (key: "Long Jump", value: workout.longJump),
-                                                (key: "Triple Jump", value: workout.tripleJump)
-                                            ]
-                                            let selectedEvent = eventOptions.filter { $0.value }.map { $0.key }.joined(separator: ", ")
-                                            if selectedEvent != "" {
-                                                Text("Event(s) Practiced: \(selectedEvent)")
+                                                // Condition
+                                                ForEach([
+                                                    (key: "Injury", value: workout.injury),
+                                                    (key: "Soreness", value: workout.soreness),
+                                                    (key: "Fatigued", value: workout.fatigued),
+                                                    (key: "Peak Form", value: workout.peakForm)
+                                                ], id: \.key) { condition in
+                                                    if condition.value {
+                                                        Text("Condition: \(condition.key)")
+                                                    }
+                                                }
+                                                // Intensity
+                                                ForEach([
+                                                    (key: "Low", value: workout.low),
+                                                    (key: "Moderate", value: workout.moderate),
+                                                    (key: "High", value: workout.high),
+                                                    (key: "Maximum", value: workout.maximum)
+                                                ], id: \.key) { intensity in
+                                                    if intensity.value {
+                                                        Text("Intensity: \(intensity.key)")
+                                                    }
+                                                }
+                                                // Field events
+                                                let eventOptions = [
+                                                    (key: "High Jump", value: workout.highJump),
+                                                    (key: "Pole Vault", value: workout.poleVault),
+                                                    (key: "Hammer Throw", value: workout.hammerThrow),
+                                                    (key: "Discus", value: workout.discus),
+                                                    (key: "Shot Put", value: workout.shotPut),
+                                                    (key: "Javelin", value: workout.javelin),
+                                                    (key: "Long Jump", value: workout.longJump),
+                                                    (key: "Triple Jump", value: workout.tripleJump)
+                                                ]
+                                                let selectedEvent = eventOptions.filter { $0.value }.map { $0.key }.joined(separator: ", ")
+                                                if selectedEvent != "" {
+                                                    Text("Event(s) Practiced: \(selectedEvent)")
+                                                }
                                             }
                                         }
                                     }
@@ -205,6 +207,28 @@ extension WorkoutData {
         dateFormatter.dateStyle = .full
         dateFormatter.timeStyle = .none
         return dateFormatter.string(from: date)
+    }
+    
+    var hasExtras: Bool {
+        // Surfaces
+        if track || indoorTrack || turf || dirt || grasshills || asphalt { return true }
+        
+        // Weather
+        if rain || snow || windy || normal || hot || cold { return true }
+        
+        // Equipment
+        if blocks || resistanceBand || weights || sled || hurdles || weightedVest || plyoBox || medicineBall || stationaryBike || treadmill { return true }
+        
+        // Condition
+        if injury || soreness || fatigued || peakForm { return true }
+        
+        // Intensity
+        if low || moderate || high || maximum { return true }
+        
+        // Field Events
+        if highJump || poleVault || hammerThrow || discus || shotPut || javelin || longJump || tripleJump { return true }
+        
+        return false
     }
 }
 
