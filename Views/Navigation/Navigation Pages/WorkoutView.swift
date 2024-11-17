7d7f646e962cdfd272af85bced41497a95cdd475
zkralec <zkralec@icloud.com>
2024-11-16 23:23:50 -0500

    Misc: Comments

--- Views/Navigation/Navigation Pages/WorkoutView.swift
+++ Views/Navigation/Navigation Pages/WorkoutView.swift
@@ -523,6 +523,7 @@ struct WorkoutView: View {
                                 }
                             }
                         } else {
+                            // Let user know how to get toggles back
                             Section("Misc.") {
                                 Text("If toggles need to be modified, make sure day is not complete.")
                                     .foregroundColor(.gray)
