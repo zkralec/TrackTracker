b7d35897a1007e24f04a4970fa5cd501585015ae
zkralec <zkralec@icloud.com>
2024-11-30 08:29:05 -0500

    Update: Basic file and comments for StarterPistol View

---
+++ Views/Events, Exercises, Log, .../StarterPistol.swift
@@ -0,0 +1,18 @@
+//
+//  StarterPistol.swift
+//  Track Tracker
+//
+//  Created by Zachary Kralec on 11/30/24.
+//
+
+import SwiftUI
+
+struct StarterPistol: View {
+    var body: some View {
+        Text("This will be a button which will play a randomly timed, automatic starter pistol sound so the user does not have to start themselves. It will say: 'On your mark, set, BANG'. It will be timed randomly so it is different every time for the user. ")
+    }
+}
+
+#Preview {
+    StarterPistol()
+}
